6122ac5590b8ec2c0af868d78148062249ff5b6f
PopovMaksim <148462617+PopovMaksim@users.noreply.github.com>
2026-04-02 08:10:05 +0000

    lab5

---
+++ students/36537/test5.py
@@ -0,0 +1,20 @@
+class MyClass:
+    pass
+
+m1 = MyClass()
+
+class Person:
+    chromosomes = 46
+
+p1 = Person()
+
+print(p1.chromosomes)
+
+
+
+class Table:
+    def __init__(self, numlegs, color):
+        self.numlegs = numlegs
+        self.color = color
+
+t1 = Table(4, 'black')
